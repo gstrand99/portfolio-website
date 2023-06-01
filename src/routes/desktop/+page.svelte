fd8125601b3041b5f91c5090717da2a721407c75
Garrett Strand <gstrand99@gmail.com>
2023-05-31 20:42:17 -0500

    continues trying to figure stuff out

--- src/routes/desktop/+page.svelte
+++ src/routes/desktop/+page.svelte
@@ -3,6 +3,15 @@
 </script>
 
 <div>
-  <p>{$userName}</p>
+   <p>{$userName}</p>
   <a href="/">home</a>
 </div>
+
+<style>
+  div {
+    width: 100%;
+    height: 100%;
+    /* background: black; */
+    position: relative; 
+  }
+</style>
